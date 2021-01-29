0a14c92783900471ab9ec6eab3c788cba3c0ce95
Lalit R Nagpal <lalit.r.nagpal@gmail.com>
2021-01-29 21:40:10 +0530

    Spring Cloud GCP Example

--- cloud-storage-example/src/main/java/gcp/example/storage/controllers/WebController.java
+++ cloud-storage-example/src/main/java/gcp/example/storage/controllers/WebController.java
@@ -3,6 +3,8 @@ package gcp.example.storage.controllers;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.spring.storage.GoogleStorageResource;
 
 @RestController
@@ -21,6 +24,8 @@ public class WebController {
 	@Value("${gcs-resource-test-bucket}")
 	private Resource gcsFile;
 	private GoogleStorageResource googleCloudStorageResource;
+	
+	Map<String,String> resourceProperties = new HashMap<>();
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String readGcsFile() throws IOException {
@@ -29,14 +34,6 @@ public class WebController {
 		// System.out.println("URI "+this.gcsFile.getURL());
 		System.out.println("Last modified: "+this.gcsFile.lastModified());
 		System.out.println("Content Length "+this.gcsFile.contentLength());
-		
-		googleCloudStorageResource = (GoogleStorageResource)this.gcsFile;
-		if (googleCloudStorageResource.bucketExists()) {
-			System.out.println("googleCloudStorageResource blob name -> " + googleCloudStorageResource.getBlobName());
-			System.out.println("googleCloudStorageResource bucket name -> " + googleCloudStorageResource.getBucketName());
-			System.out.println("googleCloudStorageResource file name -> " + googleCloudStorageResource.getFilename());
-			System.out.println("googleCloudStorageResource storage location -> " + googleCloudStorageResource.getGoogleStorageLocation());
-		}
 		
 		return StreamUtils.copyToString(
 				this.gcsFile.getInputStream(),
@@ -51,6 +48,23 @@ public class WebController {
 			os.write(data.getBytes());
 		}
 		return "file was updated\n";
+	}
+	
+	@RequestMapping(value = "/details", method = RequestMethod.GET)
+	String getDetails() throws IOException {
+		
+		googleCloudStorageResource = (GoogleStorageResource)this.gcsFile;
+		
+		if (googleCloudStorageResource.bucketExists()) {
+			
+			resourceProperties.put("googleCloudStorageResource.getBlobName", googleCloudStorageResource.getBlobName());
+			resourceProperties.put("googleCloudStorageResource.getBucketName", googleCloudStorageResource.getBucketName());
+			resourceProperties.put("googleCloudStorageResource.getFileName", googleCloudStorageResource.getFilename());
+			resourceProperties.put("googleCloudStorageResource.getStorageLocation", googleCloudStorageResource.getGoogleStorageLocation().toString());
+		}
+		
+		String json = new ObjectMapper().writeValueAsString(resourceProperties);
+		return json;		
 	}
 	
 	
