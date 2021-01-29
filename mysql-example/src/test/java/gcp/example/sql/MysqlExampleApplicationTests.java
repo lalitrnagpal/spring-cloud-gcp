129d4536a8e6ff58465429906b40756d17eb189b
Lalit R Nagpal <lalit.r.nagpal@gmail.com>
2021-01-29 22:51:23 +0530

    Spring Cloud GCP - MySQL Example

---
+++ mysql-example/src/test/java/gcp/example/sql/MysqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package gcp.example.sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
