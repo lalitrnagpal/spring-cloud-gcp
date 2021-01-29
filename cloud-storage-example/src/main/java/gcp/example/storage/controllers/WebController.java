27382dfdb73bce0451ef646ed39e463f032e9fa1
Lalit R Nagpal <lalit.r.nagpal@gmail.com>
2021-01-29 20:44:25 +0530

    Added GoogleStorageResource Example to Spring Cloud GCP - Cloud Storage Example

--- cloud-storage-example/src/main/java/gcp/example/storage/controllers/WebController.java
+++ cloud-storage-example/src/main/java/gcp/example/storage/controllers/WebController.java
@@ -13,11 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.cloud.spring.storage.GoogleStorageResource;
+
 @RestController
 public class WebController {
 	
-	@Value("gs://${gcs-resource-test-bucket}/myfile.txt")
+	@Value("${gcs-resource-test-bucket}")
 	private Resource gcsFile;
+	private GoogleStorageResource googleCloudStorageResource;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String readGcsFile() throws IOException {
@@ -26,9 +29,20 @@ public class WebController {
 		// System.out.println("URI "+this.gcsFile.getURL());
 		System.out.println("Last modified: "+this.gcsFile.lastModified());
 		System.out.println("Content Length "+this.gcsFile.contentLength());
+		
+		googleCloudStorageResource = (GoogleStorageResource)this.gcsFile;
+		if (googleCloudStorageResource.bucketExists()) {
+			System.out.println("googleCloudStorageResource blob name -> " + googleCloudStorageResource.getBlobName());
+			System.out.println("googleCloudStorageResource bucket name -> " + googleCloudStorageResource.getBucketName());
+			System.out.println("googleCloudStorageResource file name -> " + googleCloudStorageResource.getFilename());
+			System.out.println("googleCloudStorageResource storage location -> " + googleCloudStorageResource.getGoogleStorageLocation());
+		}
+		
 		return StreamUtils.copyToString(
 				this.gcsFile.getInputStream(),
 				Charset.defaultCharset()) + "\n";
+		
+		
 	}
 
 	@RequestMapping(value = "/", method = RequestMethod.POST)
@@ -38,4 +52,8 @@ public class WebController {
 		}
 		return "file was updated\n";
 	}
+	
+	
 }
+
+
